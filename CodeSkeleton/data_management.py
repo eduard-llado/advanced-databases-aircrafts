775fce0f518da5de3bc4dc757b38cf0eba02ef51
Eduard Lladó <draude1000@gmail.com>
2021-11-29 13:43:50 +0100

    Add DW connection

--- CodeSkeleton/data_management.py
+++ CodeSkeleton/data_management.py
@@ -4,29 +4,20 @@ from pyspark.sql import SQLContext
 from pyspark.sql import Row
 from pyspark.sql import SparkSession
 
-username = "nom.cognom"
-password = "DBddnnyy"
+username = "eduard.llado"
+password = "DB100200"
 
 def process(sc):
     sess = SparkSession(sc)
 
-    AMOS = (sess.read
-        .format("jdbc")
-        .option("driver","org.postgresql.Driver")
-        .option("url", "jdbc:postgresql://postgresfib.fib.upc.edu:6433/AMOS?sslmode=require")
-        .option("dbtable", "oldinstance.MaintenanceEvents")
-        .option("user", username)
-        .option("password", password)
-        .load())
+    DW = (sess.read
+       .format("jdbc")
+       .option("driver","org.postgresql.Driver")
+       .option("url", "jdbc:postgresql://postgresfib.fib.upc.edu:6433/DW?sslmode=require")
+       .option("dbtable", "public.aircraftdimension")
+       .option("user", username)
+       .option("password", password)
+       .load())
 
-    AIMS = (sess.read
-        .format("jdbc")
-        .option("driver","org.postgresql.Driver")
-        .option("url", "jdbc:postgresql://postgresfib.fib.upc.edu:6433/AIMS?sslmode=require")
-        .option("dbtable", "public.flights")
-        .option("user", username)
-        .option("password", password)
-        .load())
-
-    input = (sc.textFile("./resources/trainingData/311014-CHC-FUE-9429-XY-SHM.csv.csv")
+    input = (sc.textFile("./resources/trainingData/311014-CHC-FUE-9429-XY-SHM.csv")
         .cache())
