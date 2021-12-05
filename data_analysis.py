53442da39d116b6f2a87b088e166c0d06c2e53bf
Eduard Lladó <draude1000@gmail.com>
2021-12-05 14:42:08 +0100

    Fix column type error in data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -4,12 +4,17 @@ from pyspark.ml.feature import StringIndexer, VectorAssembler
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.sql.types import DoubleType, IntegerType
+from pyspark.sql.functions import col
 
 def analysis(sc):
     sess = SparkSession(sc)
 
-    data = sess.read.option("header", True).csv("./matrix")
-    data.show()
+    data = (sess.read.option("header", True).csv("./matrix")
+            .withColumn("SensorAVG", col("SensorAVG").cast(DoubleType()))
+            .withColumn("FH", col("FH").cast(DoubleType()))
+            .withColumn("FC", col("FC").cast(IntegerType()))
+            .withColumn("DM", col("DM").cast(IntegerType())))
 
     labelIndexer = StringIndexer(inputCol="Label", outputCol="indexedLabel").fit(data)
     vectorAssembler = VectorAssembler(inputCols=["SensorAVG", "FH", "FC", "DM"], outputCol="features")
@@ -27,4 +32,3 @@ def analysis(sc):
     accuracy = evaluator.evaluate(predictions)
     print(accuracy)
 
-    # data type string not supported FH, FC...
